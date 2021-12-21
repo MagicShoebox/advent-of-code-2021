632c8e5734dcda09e740f479fa5a5781d3a08adf
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2021-12-21 14:48:14 -0500

    Add Day 01 skeleton

---
+++ day01/Day01.hs
@@ -0,0 +1,11 @@
+module Main (main) where
+
+import Util.Input (putResult)
+
+main = putResult day01
+
+day01 input = unlines ["Part 1:", part1 input, "", "Part 2:", part2 input]
+
+part1 input = "part1"
+
+part2 input = "part2"
