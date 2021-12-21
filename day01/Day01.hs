fbce5e1f2ed3565c5ad8d1933441f2ed72d8840d
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2021-12-21 15:52:26 -0500

    Write Day 01 solution

--- day01/Day01.hs
+++ day01/Day01.hs
@@ -1,11 +1,16 @@
 module Main (main) where
 
 import Util.Input (putResult)
+import Control.Applicative (ZipList(getZipList))
 
 main = putResult day01
 
-day01 input = unlines ["Part 1:", part1 input, "", "Part 2:", part2 input]
+day01 input = unlines ["Part 1:", show (part1 measurements), "", "Part 2:", show (part2 measurements)]
+    where measurements = map read $ lines input
 
-part1 input = "part1"
+part1 = countIncreases
 
-part2 input = "part2"
+part2 = countIncreases . windows
+
+countIncreases xs = length . filter id $ zipWith (<) xs (drop 1 xs)
+windows xs = zipWith3 (\a b c -> a+b+c) xs (drop 1 xs) (drop 2 xs)
