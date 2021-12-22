1d090539b3006b5414a0c53209ebe506b44efc49
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2021-12-21 19:45:20 -0500

    Day 02 part 2

--- day02/Day02.hs
+++ day02/Day02.hs
@@ -6,23 +6,29 @@ main = showResult part1 part2
 
 part1 input = horizontal * depth
   where
-    Position horizontal depth = foldl navigate (Position 0 0) (commands input)
+    Position horizontal depth _ = foldl navigate (Position 0 0 0) (commands input)
+    navigate (Position x y _) (Command Forward d) = Position (x + d) y 0
+    navigate (Position x y _) (Command Down d) = Position x (y + d) 0
+    navigate (Position x y _) (Command Up d) = Position x (y - d) 0
 
-part2 = tbd
+part2 input = horizontal * depth
+  where
+    Position horizontal depth _ = foldl navigate (Position 0 0 0) (commands input)
+    navigate (Position x y a) (Command Forward d) = Position (x + d) (y + d * a) a
+    navigate (Position x y a) (Command Down d) = Position x y (a + d)
+    navigate (Position x y a) (Command Up d) = Position x y (a - d)
 
 type Horizontal = Int
 
 type Depth = Int
 
-data Position = Position Horizontal Depth
+type Aim = Int
+
+data Position = Position Horizontal Depth Aim
 
 data Command = Command Direction Int
 
 data Direction = Forward | Down | Up
-
-navigate (Position x y) (Command Forward d) = Position (x + d) y
-navigate (Position x y) (Command Down d) = Position x (y + d)
-navigate (Position x y) (Command Up d) = Position x (y - d)
 
 commands input = map (parseCommand . words) (lines input)
 
