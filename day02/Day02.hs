29e3f3a8212c39b2dff3c9006b9c3a6459c9c4d8
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2021-12-21 19:20:07 -0500

    Day 02 part 1

--- day02/Day02.hs
+++ day02/Day02.hs
@@ -1,9 +1,35 @@
 module Main (main) where
 
-import Util.Advent (tbd, showResult)
+import Util.Advent (showResult, tbd)
 
 main = showResult part1 part2
 
-part1 = tbd
+part1 input = horizontal * depth
+  where
+    Position horizontal depth = foldl navigate (Position 0 0) (commands input)
 
 part2 = tbd
+
+type Horizontal = Int
+
+type Depth = Int
+
+data Position = Position Horizontal Depth
+
+data Command = Command Direction Int
+
+data Direction = Forward | Down | Up
+
+navigate (Position x y) (Command Forward d) = Position (x + d) y
+navigate (Position x y) (Command Down d) = Position x (y + d)
+navigate (Position x y) (Command Up d) = Position x (y - d)
+
+commands input = map (parseCommand . words) (lines input)
+
+parseCommand [direction, distance] = Command (parseDirection direction) (read distance)
+parseCommand _ = error "Bad command"
+
+parseDirection "forward" = Forward
+parseDirection "down" = Down
+parseDirection "up" = Up
+parseDirection _ = error "Bad direction"
