e470348a54fb7a0c36b69ad6565e0ec332978096
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2021-12-22 17:10:39 -0500

    Day 03 part 2

--- day03/Day03.hs
+++ day03/Day03.hs
@@ -1,27 +1,45 @@
 module Main (main) where
 
-import Data.Bits (complement, shiftL, (.|.))
+import Data.Bits (bit, complement, shiftL, (.|.))
 import Data.List (transpose)
 import Util.Advent (showResult, tbd)
+import Debug.Trace (traceShow)
 
 main = showResult part1 part2
 
-part1 :: String -> Int
-part1 input = gamma diagsT * epsilon diagsT
+part1 input = gamma diags * epsilon diags
   where
-    diagsT = transpose (diagnostics input)
+    diags = diagnostics input
 
-part2 = tbd
-
-diagnostics input = map (map parseDigit) (lines input)
-
-gamma = commonBinary 0
-
-epsilon = complement . commonBinary (-1)
-
-commonBinary n numbers = foldl binaryReducer n $ zip (map sum numbers) (map length numbers)
+part2 input = oxygen diags * co2 diags
   where
-    binaryReducer g (s, l) = (g `shiftL` 1) .|. (if s > l `div` 2 then 1 else 0)
+    diags = diagnostics input
+
+diagnostics = transpose . map (map parseDigit) . lines
+
+gamma = binary . map commonDigit
+
+epsilon = binary . map ((1-) . commonDigit)
+
+oxygen = binary . filterDigits commonDigit
+
+co2 = binary . filterDigits ((1-) . commonDigit)
+
+filterDigits selector [] = []
+filterDigits selector ([] : xss) = []
+filterDigits selector ([x] : xss) = x : filterDigits selector xss
+filterDigits selector (xs : xss) = digit : filterDigits selector (map filterMatches xss)
+  where
+    digit = selector xs
+    filterMatches = filterWhen [x == digit | x <- xs]
+    filterWhen bs xs = [x | (b, x) <- zip bs xs, b]
+
+binary = binary' . reverse
+
+binary' [] = 0
+binary' (x : xs) = x .|. binary' xs `shiftL` 1
+
+commonDigit xs = if 2 * sum xs >= length xs then 1 else 0
 
 parseDigit '0' = 0
 parseDigit '1' = 1
