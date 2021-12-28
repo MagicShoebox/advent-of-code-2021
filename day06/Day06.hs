d6e7b5d3a64f63841b58a69fda303b778eaac7ff
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2021-12-28 18:36:14 -0500

    Day 06 part 1

--- day06/Day06.hs
+++ day06/Day06.hs
@@ -1,10 +1,26 @@
 module Main (main) where
 
+import Data.Maybe (fromMaybe)
+import Data.Tuple (swap)
 import Debug.Trace (traceShow)
 import Util.Advent (showResult, tbd)
+import Util.List (frequency)
+import Util.String (split)
 
 main = showResult part1 part2
 
-part1 = tbd
+part1 input = sum [pop age 80 | age <- [0 .. 8]]
+  where
+    pop = population (parseInput input)
 
 part2 = tbd
+
+population initial age day
+  | day < 0 = 0
+  | day == 0 = fromMaybe 0 (lookup age initial)
+  | age == 6 = population initial 7 (day - 1) + population initial 0 (day - 1)
+  | age == 8 = population initial 0 (day - 1)
+  | otherwise = population initial (age + 1) (day - 1)
+
+parseInput :: String -> [(Int, Int)]
+parseInput = map swap . frequency . map read . split ","
