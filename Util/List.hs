02b808bf05bf3459a2c4d929103f4c0d36899025
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2021-12-31 16:22:38 -0500

    Day 08 part 2

--- Util/List.hs
+++ Util/List.hs
@@ -1,8 +1,12 @@
-module Util.List (frequency) where
+module Util.List (frequency, groupOn) where
 
 import Control.Arrow ((&&&))
-import Data.List (group, sort)
+import Data.List (group, sort, groupBy)
+import Data.Function (on)
 
 -- https://stackoverflow.com/a/26372259/3491874
 frequency :: Ord a => [a] -> [(Int, a)]
 frequency = map (length &&& head) . group . sort
+
+groupOn :: Eq a => (b -> a) -> [b] -> [[b]]
+groupOn k = groupBy ((==) `on` k)
--- day08/Day08.hs
+++ day08/Day08.hs
@@ -1,15 +1,37 @@
 module Main (main) where
 
-import Control.Arrow (second)
+import Data.Bifunctor (bimap)
+import Data.List (elemIndex, intersect, sort, sortOn, union, (\\))
 import Debug.Trace (traceShow)
 import Util.Advent (showResult, tbd)
+import Util.List (groupOn)
 
 main = showResult part1 part2
 
--- Digits 1, 4, 7, 8 use 2, 4, 3, 7 segments respectively
-part1 input = length $ filter (`elem` [2, 3, 4, 7]) $ map length digits
-    where digits = concatMap snd (parseInput input)
+part1 input = length $ filter ((`elem` [2, 3, 4, 7]) . length) digits
+  where
+    digits = concatMap snd (parseInput input)
 
-part2 = tbd
+part2 input = sum <$> traverse (fmap fromDigits . decode) (parseInput input)
+  where
+    decode (patterns, digits) = mapM (`elemIndex` getMap patterns) digits
+    fromDigits = foldl (\a b -> a * 10 + b) 0
 
-parseInput = map (second (drop 1) . splitAt 10 . words) . lines
+getMap [[one], [seven], [four], twothreefive, zerosixnine, [eight]] = map sort [abcefg, cf, acdeg, acdfg, bcdf, abdfg, abdefg, acf, abcdefg, abcdfg]
+  where
+    cf = one
+    acf = seven
+    bcdf = four
+    abcdefg = eight
+    abfg = foldl1 intersect zerosixnine
+    adg = foldl1 intersect twothreefive
+    cde = abcdefg \\ abfg
+    abcefg = abcdefg \\ (adg `intersect` cde)
+    acdeg = adg `union` cde
+    acdfg = adg `union` cf
+    abdfg = adg `union` abfg
+    abdefg = abcdefg \\ (cf `intersect` cde)
+    abcdfg = abfg `union` bcdf
+getMap _ = error "Bad input"
+
+parseInput = map (bimap (groupOn length . sortOn length) (map sort . drop 1) . splitAt 10 .  words) . lines
