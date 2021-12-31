3b3f153e663b67728c890b68edc66c6e815d3be6
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2021-12-30 21:19:03 -0500

    Day 07 part 1

--- day07/Day07.hs
+++ day07/Day07.hs
@@ -2,9 +2,17 @@ module Main (main) where
 
 import Debug.Trace (traceShow)
 import Util.Advent (showResult, tbd)
+import Util.String (split)
 
 main = showResult part1 part2
 
-part1 = tbd
+part1 input = minimum $ fuel xs
+  where
+    xs = parseInput input
 
 part2 = tbd
+
+fuel xs = [sum $ map (abs . (pos -)) xs | pos <- [(minimum xs) .. (maximum xs)]]
+
+parseInput :: String -> [Int]
+parseInput = map read . split ","
