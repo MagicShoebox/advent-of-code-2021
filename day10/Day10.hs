2033e7893c86badc292c026a4666569670463277
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2022-01-03 14:22:51 -0500

    Day 10 part 1

--- day10/Day10.hs
+++ day10/Day10.hs
@@ -1,10 +1,48 @@
 module Main (main) where
 
+import Control.Monad (join, void)
+import Data.List ((\\))
+import Data.Maybe (fromJust)
 import Debug.Trace (traceShow)
+import Text.ParserCombinators.ReadP (ReadP, char, choice, eof, get, many, many1, readP_to_S, satisfy, (<++))
 import Util.Advent (showResult, tbd)
+
+data Valid = Valid
+
+data ParseError = Incomplete Char | Corrupt Char
+
+type ParseResult = Either ParseError Valid
 
 main = showResult part1 part2
 
-part1 = tbd
+part1 input = sum $ map score corrupt
+  where
+    errors = [x | (Left x) <- parseInput input]
+    corrupt = [x | (Corrupt x) <- errors]
+    score ')' = 3
+    score ']' = 57
+    score '}' = 1197
+    score '>' = 25137
+    score _ = undefined
 
 part2 = tbd
+
+tokens = [('[', ']'), ('(', ')'), ('{', '}'), ('<', '>')]
+
+parseLine = sequence <$> many1 group
+
+group = do
+  start <- choice $ map (char . fst) tokens
+  many group >>= either (return . Left) (const $ incomplete start <++ valid start <++ corrupt start) . sequence
+  where
+    valid s = do
+      char (fromJust $ lookup s tokens)
+      return $ Right Valid
+    incomplete s = do
+      eof
+      return $ Left $ Incomplete s
+    corrupt s = do
+      e <- satisfy (`elem` map snd tokens \\ [s])
+      return $ Left $ Corrupt e
+
+parseInput = map (traverse fst . readP_to_S parseLine) . lines
