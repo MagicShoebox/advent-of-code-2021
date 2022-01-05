b7502991e4ad1859f62fa6952c9ebca816cd458f
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2022-01-05 14:32:25 -0500

    Day 13 part 2

--- day13/Day13.hs
+++ day13/Day13.hs
@@ -1,19 +1,25 @@
 module Main (main) where
 
 import Control.Arrow (Arrow (second))
+import Data.Char (chr)
 import Data.Containers.ListUtils (nubOrd)
 import Debug.Trace (traceShow)
-import Util.Advent (showResult, tbd)
+import Util.Advent (putResult, tbd)
 
 data Fold = Horizontal Int | Vertical Int deriving (Show)
 
-main = showResult part1 part2
+main = putResult part1 part2
 
-part1 input = length $ fold dots (head folds)
+part1 input = show $ length $ fold dots (head folds)
   where
     (dots, folds) = parseInput input
 
-part2 = tbd
+part2 input = unlines [[output (x, y) | x <- [0 .. width]] | y <- [0 .. height]]
+  where
+    output (x, y) = if (x, y) `elem` final then chr 9608 else ' '
+    (width, height) = both maximum $ unzip final
+    final = foldl fold dots folds
+    (dots, folds) = parseInput input
 
 fold dots f = nubOrd $ map (pos f) dots
   where
@@ -24,12 +30,13 @@ fold dots f = nubOrd $ map (pos f) dots
       | py < fy = (px, py)
       | otherwise = (px, 2 * fy - py)
 
+both f (x, y) = (f x, f y)
+
 parseInput input = (dots, folds)
   where
     breakOn x = second (drop 1) . break (== x)
     (dotLines, foldLines) = breakOn "" $ lines input
     dots = map (both read . breakOn ',') dotLines
-    both f (x, y) = (f x, f y)
     folds = map (parseFold . drop (length "fold along ")) foldLines
     parseFold ('x' : '=' : pos) = Horizontal $ read pos
     parseFold ('y' : '=' : pos) = Vertical $ read pos
