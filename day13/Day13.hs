4cc2be6ee3d02a6389aa1f7d3410687577a506bc
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2022-01-05 14:03:22 -0500

    Day 13 part 1

--- day13/Day13.hs
+++ day13/Day13.hs
@@ -1,10 +1,36 @@
 module Main (main) where
 
+import Control.Arrow (Arrow (second))
+import Data.Containers.ListUtils (nubOrd)
 import Debug.Trace (traceShow)
 import Util.Advent (showResult, tbd)
 
+data Fold = Horizontal Int | Vertical Int deriving (Show)
+
 main = showResult part1 part2
 
-part1 = tbd
+part1 input = length $ fold dots (head folds)
+  where
+    (dots, folds) = parseInput input
 
 part2 = tbd
+
+fold dots f = nubOrd $ map (pos f) dots
+  where
+    pos (Horizontal fx) (px, py)
+      | px < fx = (px, py)
+      | otherwise = (2 * fx - px, py)
+    pos (Vertical fy) (px, py)
+      | py < fy = (px, py)
+      | otherwise = (px, 2 * fy - py)
+
+parseInput input = (dots, folds)
+  where
+    breakOn x = second (drop 1) . break (== x)
+    (dotLines, foldLines) = breakOn "" $ lines input
+    dots = map (both read . breakOn ',') dotLines
+    both f (x, y) = (f x, f y)
+    folds = map (parseFold . drop (length "fold along ")) foldLines
+    parseFold ('x' : '=' : pos) = Horizontal $ read pos
+    parseFold ('y' : '=' : pos) = Vertical $ read pos
+    parseFold _ = undefined
