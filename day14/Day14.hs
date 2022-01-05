cba1867da3fe75ba4c4c75a1e3fd521a1ad411d0
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2022-01-05 15:27:14 -0500

    Day 14 part 1

--- day14/Day14.hs
+++ day14/Day14.hs
@@ -1,10 +1,28 @@
 module Main (main) where
 
+import Control.Arrow ((&&&))
+import Data.List (sort)
+import Data.Maybe (fromJust)
+import Data.Tuple (swap)
 import Debug.Trace (traceShow)
-import Util.Advent (putResult, tbd)
+import Util.Advent (showResult, tbd)
+import Util.List (frequency)
 
-main = putResult part1 part2
+main = showResult part1 part2
 
-part1 = tbd
+part1 input = uncurry (-) $ (maximum &&& minimum) $ map fst $ frequency final
+  where
+    final = iterate (step rules) template !! 10
+    (template, rules) = parseInput input
 
 part2 = tbd
+
+step rules template = inserted ++ [last template]
+  where
+    inserted = concat $ zipWith insert template $ drop 1 template
+    insert a b = [a, fromJust $ lookup (a, b) rules]
+
+parseInput input = (template, rules)
+  where
+    (template : _ : ruleLines) = lines input
+    rules = map (\(a : b : cs) -> ((a, b), last cs)) ruleLines
