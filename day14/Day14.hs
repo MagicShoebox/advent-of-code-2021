715051faf0677dd3c0d942e70dc8ecddd83d94bb
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2022-01-06 10:57:38 -0500

    Day 14 part 2

--- day14/Day14.hs
+++ day14/Day14.hs
@@ -1,28 +1,44 @@
 module Main (main) where
 
 import Control.Arrow ((&&&))
-import Data.List (sort)
-import Data.Maybe (fromJust)
-import Data.Tuple (swap)
-import Debug.Trace (traceShow)
+import Data.Array (accum, array, assocs, elems, listArray, (!), (//))
+import Debug.Trace (traceShow, traceShowId)
 import Util.Advent (showResult, tbd)
 import Util.List (frequency)
 
 main = showResult part1 part2
 
-part1 input = uncurry (-) $ (maximum &&& minimum) $ map snd $ frequency final
+part1 input = range $ filter (> 0) $ elems freqs
   where
-    final = iterate (step rules) template !! 10
+    freqs = roots (branch rules) 10 template
     (template, rules) = parseInput input
 
-part2 = tbd
-
-step rules template = inserted ++ [last template]
+part2 input = range $ filter (> 0) $ elems freqs
   where
-    inserted = concat $ zipWith insert template $ drop 1 template
-    insert a b = [a, fromJust $ lookup (a, b) rules]
+    freqs = roots (branch rules) 40 template
+    (template, rules) = parseInput input
 
-parseInput input = (template, rules)
+initial = listArray ('A', 'Z') (repeat 0)
+
+range = uncurry (-) . (maximum &&& minimum)
+
+merge arr1 arr2 = accum (+) arr1 $ assocs arr2
+
+roots f n (x : y : xs) = f (n, x, y) `merge` roots f n (y : xs)
+roots f n (x : xs) = initial // [(x, 1)]
+roots f n [] = initial
+
+branch rules = (!) memo
+  where
+    memo = array ((0, 'A', 'A'), (40, 'Z', 'Z')) [((n, x, y), branch' x y n) | n <- [0 .. 40], x <- ['A' .. 'Z'], y <- ['A' .. 'Z']]
+    branch' x z 0 = initial // [(x, 1)]
+    branch' x z n = left `merge` right
+      where
+        y = rules ! (x, z)
+        left = memo ! (n - 1, x, y)
+        right = memo ! (n - 1, y, z)
+
+parseInput input = (template, array (('A', 'A'), ('Z', 'Z')) rules)
   where
     (template : _ : ruleLines) = lines input
     rules = map (\(a : b : cs) -> ((a, b), last cs)) ruleLines
