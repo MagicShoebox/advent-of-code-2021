818aad3364e0ad5fbbe3c480d9929ef2ec796464
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2022-01-10 16:48:17 -0500

    Day 17 (both) (ugly)

--- day17/Day17.hs
+++ day17/Day17.hs
@@ -1,7 +1,7 @@
-{-# LANGUAGE TupleSections #-}
-
 module Main (main) where
 
+import Control.Applicative (liftA2)
+import Data.List (find)
 import Debug.Trace (traceShow, traceShowId)
 import Text.Regex.PCRE ((=~))
 import Util.Advent (showResult, tbd)
@@ -14,15 +14,14 @@ part1 input = triangle $ abs (y1 area) - 1
   where
     area = parseInput input
 
-part2 input = concatMap (\vy -> map (,vy) (hitsArea area vy)) [-200 .. 200]
+part2 input = length $ filter (hitsArea area) $ liftA2 (,) [-500 .. 500] [-500 .. 500]
   where
     area = parseInput input
 
-hitsArea area vy = filter hits [vx1 .. vx2]
+hitsArea area (vx, vy) = maybe False inArea end
   where
-    (t1, t2) = tBounds vy (y1 area) (y2 area)
-    (vx1, vx2) = vxBounds (x1 area) (x2 area)
-    hits vx = any inArea [(disp vx t, height vy t) | t <- [t1 .. t2]]
+    end = find (\(x, y) -> inArea (x, y) || (y < 0 && y < y1 area)) path
+    path = [(disp vx t, height vy t) | t <- [0 ..]]
     inArea (x, y) = x >= x1 area && x <= x2 area && y >= y1 area && y <= y2 area
 
 disp vx t =
@@ -31,17 +30,6 @@ disp vx t =
    in (vx * t') `op` triangle (t' - 1)
 
 height vy t = vy * t - triangle (t - 1)
-
-vxBounds x1 x2 = (ceiling $ ideal $ fromIntegral x1, floor $ ideal $ fromIntegral x2)
-  where
-    ideal x
-      | x >= 0 = sqrt (1 + 8 * x) / 2 - (1 / 2)
-      | otherwise = - ideal (- x)
-
-tBounds vy y1 y2 = (ceiling $ ideal $ fromIntegral y2, floor $ ideal $ fromIntegral y1)
-  where
-    vy' = fromIntegral vy
-    ideal h = (vy' + 1 / 2) + sqrt (vy' * vy' + vy' - (2 * h) + 1 / 4)
 
 triangle x = (x * (x + 1)) `div` 2
 
