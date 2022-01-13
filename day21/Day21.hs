a5d7c76e7df4b6ae5179c2a4cc6c921ab1e47d25
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2022-01-13 16:51:19 -0500

    Day 21 part 1

--- day21/Day21.hs
+++ day21/Day21.hs
@@ -1,10 +1,32 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main (main) where
 
+import Control.Arrow (second, (&&&))
+import Data.Char (digitToInt)
+import Data.List (findIndex)
+import Data.Maybe (fromJust)
 import Debug.Trace (trace, traceShow, traceShowId)
 import Util.Advent (showResult, tbd)
 
 main = showResult part1 part2
 
-part1 = tbd
+part1 input = losingScore * (rolls * 6 + 3)
+  where
+    rolls = uncurry min $ both (subtract 1 . fromJust . findIndex (>= 1000)) (p1scores, p2scores)
+    losingScore = min (p1scores !! rolls) (p2scores !! rolls)
+    p1scores = scores $ positions p1movements p1start
+    p2scores = scores $ positions p2movements p2start
+    (p1start, p2start) = parseInput input
 
 part2 = tbd
+
+scores = scanl (+) 0 . map (\case 0 -> 10; x -> x) . tail
+
+positions movements start = scanl (\a b -> (a + b) `mod` 10) start movements
+
+(p1movements, p2movements) = both cycle ([6, 4, 2, 0, 8], [5, 3, 1, 9, 7])
+
+both f (a, b) = (f a, f b)
+
+parseInput input = both (digitToInt . last) $ (head &&& head . tail) $ lines input
