c54b394c856e6c4eab6d141e59ee050c4df25799
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2022-02-01 21:33:17 -0500

    Day 24 attempt 5

--- day24/Day24.hs
+++ day24/Day24.hs
@@ -1,61 +1,107 @@
 module Main (main) where
 
-import Control.Monad.Trans.State (State, evalState, gets, modify)
+import Control.Monad.Trans.State (State, evalState, execState, get, gets, modify, put)
 import Data.Array (Array, accum, elems, listArray, (!), (//))
 import Data.Char (digitToInt)
 import Data.Maybe (catMaybes, mapMaybe)
-import Data.Set (Set, empty, member, singleton, union)
+import Data.Set (Set, empty, fromList, intersection, member, singleton, size, toList, union)
 import Debug.Trace (trace, traceShow, traceShowId)
 import Util.Advent (showResult, tbd)
 
 type Reg = Char
 
-type Op = Int -> Int -> Int
+data Op = Add | Mul | Div | Mod | Eql
 
-data Instr = Input Reg | OpReg Op Reg Reg | OpLit Op Reg Int | End
+data Node = Node Term (Set Int)
+
+data Term = Input Int | Literals | Instr Op Node Node
 
 main = showResult part1 part2
 
-part1 input = maximumValid instructions
+part1 input = [trace (show idx ++ ":" ++ show val ++ ":") traceShowId $ check instructions idx val | idx <- [13, 12 .. 0], val <- [9, 8 .. 1]]
   where
     instructions = parseInput input
 
 part2 = tbd
 
-maximumValid instructions = evalState (execute initialRegs 0) cache
-  where
-    initialRegs = listArray ('w', 'z') (repeat 0)
-    cache = fmap (const empty) instructions
-    execute regs n = do
-      cached <- gets (! n)
-      if (r1, r2, r3, r4) `member` cached
-        then return Nothing
-        else do
-          result <- evaluate (instructions ! n)
-          modify (\s -> accum union s [(n, singleton (r1, r2, r3, r4))])
-          return result
-      where
-        [r1, r2, r3, r4] = elems regs
-        evaluate (OpReg op d s) = execute (regs // [(d, op (regs ! d) (regs ! s))]) (n + 1)
-        evaluate (OpLit op d l) = execute (regs // [(d, op (regs ! d) l)]) (n + 1)
-        evaluate (Input r) = headMaybe . catMaybes <$> mapM (\i -> fmap (i :) <$> execute (regs // [(r, if n < 80 then trace (show n ++ ":" ++ show i) i else i)]) (n + 1)) [9, 8 .. 1]
-        evaluate End = return $ if regs ! 'z' == 0 then Just [] else Nothing
+instance Show Node where
+  show (Node (Input x) _) = "s[" ++ show x ++ "]"
+  show (Node Literals xs) = show (toList xs)
+  show (Node (Instr op left right) xs) = "(" ++ show left ++ " " ++ show op ++ " " ++ show right ++ ")"
 
-parseInput input = listArray (0, length lines') instructions
+instance Show Op where
+  show Add = "+"
+  show Mul = "*"
+  show Div = "/"
+  show Mod = "%"
+  show Eql = "=="
+
+check instructions index value = let (Node _ values) = check' instructions in 0 `member` values
   where
-    lines' = lines input
-    instructions = map (parseInstruction . words) lines' ++ [End]
-    parseInstruction ["inp", reg : _] = Input reg
-    parseInstruction [op, reg : _, term@(t : _)]
-      | t `elem` ['w' .. 'z'] = OpReg (parseOp op) reg t
-      | otherwise = OpLit (parseOp op) reg (read term)
-    parseInstruction _ = undefined
-    parseOp "add" = (+)
-    parseOp "mul" = (*)
-    parseOp "div" = div
-    parseOp "mod" = mod
-    parseOp "eql" = \a b -> if a == b then 1 else 0
-    parseOp _ = undefined
+    check' (Node (Input x) values)
+      | x == index = Node (Input x) (singleton value)
+      | otherwise = Node (Input x) values
+    check' (Node (Instr op left right) values) = optimize op (check' left) (check' right)
+    check' literals = literals
+
+optimize op (Node Literals left) (Node Literals right) = Node Literals $ combine op left right
+optimize Add left@(Node _ leftValues) right@(Node _ rightValues)
+  | all (== 0) leftValues = right
+  | all (== 0) rightValues = left
+optimize Mul left@(Node _ leftValues) right@(Node _ rightValues)
+  | all (== 1) leftValues = right
+  | all (== 1) rightValues = left
+optimize Mul (Node _ leftValues) (Node _ rightValues)
+  | all (== 0) leftValues = Node Literals (singleton 0)
+  | all (== 0) rightValues = Node Literals (singleton 0)
+optimize Div left (Node _ rightValues)
+  | all (== 1) rightValues = left
+optimize Div (Node _ leftValues) (Node _ rightValues)
+  | maximum leftValues < minimum rightValues = Node Literals (singleton 0)
+optimize Mod left@(Node _ leftValues) (Node _ rightValues)
+  | maximum leftValues < minimum rightValues = left
+optimize Eql (Node _ leftValues) (Node _ rightValues)
+  | null (leftValues `intersection` rightValues) = Node Literals (singleton 0)
+optimize op (Node left@Instr {} leftValues) (Node right@Instr {} rightValues) = Node (Instr op (Node left empty) (Node right empty)) (combine op leftValues rightValues)
+optimize op (Node left@Instr {} leftValues) (Node right rightValues) = Node (Instr op (Node left empty) (Node right rightValues)) (combine op leftValues rightValues)
+optimize op (Node left leftValues) (Node right@Instr {} rightValues) = Node (Instr op (Node left leftValues) (Node right empty)) (combine op leftValues rightValues)
+optimize op (Node left leftValues) (Node right rightValues) = Node (Instr op (Node left leftValues) (Node right rightValues)) (combine op leftValues rightValues)
+
+combine op left right = fromList [doOp op x y | x <- toList left, y <- toList right]
+
+parseInput input = finalRegs ! 'z'
+  where
+    (_, finalRegs) = execState instructions initial
+    instructions = mapM (parseInstruction . words) $ lines input
+    initial = (0, listArray ('w', 'z') (repeat $ Node Literals $ singleton 0))
+
+parseInstruction ["inp", reg : _] = do
+  (idx, regs) <- get
+  let values = fromList [1 .. 9]
+  let term = Input idx
+  put (idx + 1, regs // [(reg, Node term values)])
+parseInstruction [op, reg : _, term@(t : _)] = do
+  (idx, regs) <- get
+  let isReg c = c == 'w' || c == 'x' || c == 'y' || c == 'z'
+  let left = regs ! reg
+  let right = if isReg t then regs ! t else Node Literals $ singleton (read term)
+  let reg' = optimize (parseOp op) left right
+  let regs' = regs // [(reg, reg')]
+  put (idx, regs')
+parseInstruction _ = undefined
+
+parseOp "add" = Add
+parseOp "mul" = Mul
+parseOp "div" = Div
+parseOp "mod" = Mod
+parseOp "eql" = Eql
+parseOp _ = undefined
+
+doOp Add = (+)
+doOp Mul = (*)
+doOp Div = div
+doOp Mod = mod
+doOp Eql = \a b -> if a == b then 1 else 0
 
 headMaybe [] = Nothing
 headMaybe (x : _) = Just x
